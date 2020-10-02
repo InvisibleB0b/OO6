a115dccdcc3f51aef12724dbe5f2f44f73785987
InvisibleB0b <theodor.per@gmail.com>
2020-10-02 11:23:54 +0200

    Add project files.

---
+++ OO6/Server.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OO6
+{
+    class Server
+    {
+
+        public static void Start()
+        {
+
+            try
+            {
+                TcpListener server = null;
+
+                IPAddress localAddress = null;
+                var host = Dns.GetHostEntry(Dns.GetHostName());
+                foreach (var ip in host.AddressList)
+                {
+                    if (ip.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        Console.WriteLine(ip.ToString());
+
+                        localAddress = IPAddress.Parse(ip.ToString());
+
+                    }
+                }
+
+                int port = 4646;
+
+
+                server = new TcpListener(IPAddress.Loopback, port);
+
+                server.Start();
+
+                Console.WriteLine("Waiting for a connection........");
+
+
+
+
+                while (true)
+                {
+
+
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+
+                    Task.Run(() =>
+                    {
+                        TcpClient tempSocket = client;
+                        DoClient(tempSocket);
+                    });
+
+
+                }
+
+                server.Stop();
+                Console.WriteLine("server stopped");
+
+
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+
+        }
+
+        public static void DoClient(TcpClient socket)
+        {
+            Stream ns = socket.GetStream();
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true;
+
+            sw.WriteLine("You are connected!!!");
+            int numOfWords = 0;
+
+            while (true)
+            {
+
+
+
+                string message = sr.ReadLine();
+
+                if (message.ToLower().Contains("luk"))
+                {
+                    break;
+                }
+
+                numOfWords += message.Split(" ").Length;
+
+                Console.WriteLine("Received message : " + message);
+
+
+                sw.WriteLine(message + $" number of send words {numOfWords}");
+
+
+
+
+
+
+            }
+            sw.WriteLine("Luk");
+
+            ns.Close();
+
+            Console.WriteLine("net stream closed");
+
+            socket.Close();
+            Console.WriteLine("client closed");
+        }
+    }
+}
